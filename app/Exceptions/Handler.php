d9968eef6e2a4fe458545b43bbd08b7dbaa5f56b
aya <aya@gmail.com>
2022-06-26 12:39:04 +0200

    handel the ecxeptions

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,7 +4,7 @@ namespace App\Exceptions;
 
 use Exception;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
-
+use Illuminate\Http\Response;
 class Handler extends ExceptionHandler
 {
     /**
@@ -48,6 +48,21 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+        if ($request->expectsJson()){
+            if ($exception instanceof ModelNotFoundException) {
+                return response()->json([
+                    'errors'=>'Product Model not found'
+                ],404);
+               }
+        
+        
+            if ($exception instanceof NotFoundHttpException) {
+                return response()->json([
+                    'errors'=>'Incorect route'
+                ],Response::HTTP_NOT_FOUND);
+               }
+        
+            }
         return parent::render($request, $exception);
     }
 }
