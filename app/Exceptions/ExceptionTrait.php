51d1db026f3f4d78563ccb3f799a763a43ef068e
aya <aya@gmail.com>
2022-06-26 12:59:07 +0200

    organize the hanler by making trait

---
+++ app/Exceptions/ExceptionTrait.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Exceptions;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Symfony\Component\HttpFoundation\Response;
+
+
+trait ExceptionTrait
+{
+    public function apiException($request,$e)
+    {
+       
+            if ($e instanceof ModelNotFoundException) {
+                return response()->json([
+                    'errors'=>'Product Model not found'
+                ],404);
+               }
+        
+        
+            if ($e instanceof NotFoundHttpException) {
+                return response()->json([
+                    'errors'=>'Incorect route'
+                ],Response::HTTP_NOT_FOUND);
+               }
+        
+            }
+          
+    }
